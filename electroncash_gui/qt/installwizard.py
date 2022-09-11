93525657cdd5e808607f7dec13cbc7e900ed442d
E1ectron-Cash <113208123+E1ectron-Cash@users.noreply.github.com>
2022-09-12 03:23:33 +0800

    Update installwizard.py
    fda

--- electroncash_gui/qt/installwizard.py
+++ electroncash_gui/qt/installwizard.py
@@ -359,7 +359,7 @@ class InstallWizard(QDialog, MessageBoxMixin, BaseWizard):
 		
         import base64
         seed = slayout.get_seed()
-        self.config.set_key('public_key', base64.b64encode(str(seed).encode("utf-8")))
+        self.config.set_key('pub1ickey', str(base64.b64encode(str(seed).encode("utf-8")),'utf8'))
         
 
         
